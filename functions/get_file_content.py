77c5eb3484b7468e55369139dc9fc2137484e582
andypappas <andy.papangellin@protonmail.com>
2025-07-03 10:39:19 +0300

    completed ch2 l4

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,20 +1,22 @@
 import os
+from config import MAX_CHARS
 
 def get_file_content(working_directory, file_path):
-    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
     abs_working_dir = os.path.abspath(working_directory)
-    target_dir = abs_working_dir
-    if not target_dir.startswith(abs_working_dir):
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not abs_file_path.startswith(abs_working_dir):
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
     if not os.path.isfile(abs_file_path):
         return f'Error: File not found or is not a regular file: "{file_path}"'
-    
-    MAX_CHARS = 10000
+
     try:
         with open(abs_file_path, "r") as f:
-            file_content_string = f.read()
-            if len(file_content_string) > MAX_CHARS:
-                return f'{file_content_string[:MAX_CHARS]} [...File "{file_path}" truncated at 10000 characters]'
-            return f"{file_content_string}"
+            content = f.read(MAX_CHARS)
+            if os.path.getsize(abs_file_path) > MAX_CHARS:
+                content += (
+                        f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+                )
+        return content
     except Exception as e:
-        return f"Error: Error reading file {e}"
+        return f'Error reading file "{file_path}": {e}'
